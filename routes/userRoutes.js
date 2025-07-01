78175980b0f427b1f726f4247adb91b67796f013
kevyamon <gamekevy@gmail.com>
2025-07-01 20:07:28 +0000

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,59 @@
+import express from 'express';
+const router = express.Router();
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+// @desc    Auth user & get token (Login)
+// @route   POST /api/users/login
+// @access  Public
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).send('Invalid email or password');
+  }
+});
+
+// @desc    Register a new user
+// @route   POST /api/users/register
+// @access  Public
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).send('User already exists');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).send('Invalid user data');
+  }
+});
+
+export default router;
