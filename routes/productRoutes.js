5d644a601b139ec38b69af59c5574c7b493c0b83
kevyamon <gamekevy@gmail.com>
2025-07-01 20:06:44 +0000

    Create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,76 @@
+import express from 'express';
+const router = express.Router();
+import Product from '../models/productModel.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get('/', async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Product not found');
+  }
+});
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+router.post('/', protect, admin, async (req, res) => {
+    const { name, price, description, image, countInStock } = req.body;
+    const product = new Product({
+        name,
+        price,
+        description,
+        image,
+        countInStock,
+    });
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+router.put('/:id', protect, admin, async (req, res) => {
+    const { name, price, description, image, countInStock } = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.countInStock = countInStock;
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404).send('Product not found');
+    }
+});
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+router.delete('/:id', protect, admin, async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        await product.deleteOne();
+        res.json({ message: 'Product removed' });
+    } else {
+        res.status(404).send('Product not found');
+    }
+});
+
+
+export default router;
