7903cbe44fb894199b7f2da0ff90417518fd3145
kevyamon <gamekevy@gmail.com>
2025-07-01 20:01:10 +0000

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
