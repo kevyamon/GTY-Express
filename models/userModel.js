2dd9ee182a299fadee5791ea0a7a5f750b06bb67
kevyamon <gamekevy@gmail.com>
2025-07-01 20:03:02 +0000

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -6,3 +6,26 @@ const userSchema = mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Chiffrer le mot de passe avant de sauvegarder l'utilisateur
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Méthode pour comparer le mot de passe entré avec celui en base de données
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
