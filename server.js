80c0bdf1163f8cb73d223facc5e9bf70fc27cc50
kevyamon <gamekevy@gmail.com>
2025-07-02 23:35:04 +0000

    Améliorations

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import path from 'path'; // Importer path
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -7,14 +7,18 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js'; // Importer la nouvelle route
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+// --- MODIFICATION ICI ---
+// On augmente la limite de taille pour les requêtes JSON et URL-encoded
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -23,10 +27,10 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes); // Utiliser la nouvelle route
+app.use('/api/upload', uploadRoutes);
 
-const __dirname = path.resolve(); // Rendre __dirname disponible
-app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // Rendre le dossier 'uploads' statique
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
