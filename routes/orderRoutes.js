9e669a8396025744a24da095e29d49b1be5749a3
kevyamon <gamekevy@gmail.com>
2025-07-04 04:08:43 +0000

    Am

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,44 +1,69 @@
 import express from 'express';
 const router = express.Router();
 import Order from '../models/orderModel.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 router.post('/', protect, async (req, res) => {
-  const { orderItems, shippingAddress, totalPrice } = req.body;
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
   if (orderItems && orderItems.length === 0) {
-    return res.status(400).send('No order items');
+    res.status(400);
+    throw new Error('No order items');
   } else {
     const order = new Order({
-      orderItems,
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
       user: req.user._id,
       shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
       totalPrice,
     });
+
     const createdOrder = await order.save();
+
     res.status(201).json(createdOrder);
   }
 });
 
-
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
-// @access  Private
 router.get('/myorders', protect, async (req, res) => {
     const orders = await Order.find({ user: req.user._id });
     res.json(orders);
 });
 
-
-// @desc    Get all orders
-// @route   GET /api/orders
-// @access  Private/Admin
-router.get('/', protect, admin, async (req, res) => {
-    const orders = await Order.find({}).populate('user', 'id name');
-    res.json(orders);
-});
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+router.get('/:id', protect, async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    );
+  
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  });
 
 export default router;
