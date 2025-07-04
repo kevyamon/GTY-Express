4a7f9183db7219137dc99a2bd09c76975a38dfaa
kevyamon <gamekevy@gmail.com>
2025-07-04 00:01:06 +0000

    Améliorations

--- models/productModel.js
+++ models/productModel.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 const productSchema = mongoose.Schema(
   {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     name: { type: String, required: true },
     image: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true, default: 0 },
+    originalPrice: { type: Number }, // Assurez-vous que ce champ est bien là
     countInStock: { type: Number, required: true, default: 0 },
   },
   {
