f02be5ecbd0d746cb9ce3f6d4a3c228d67f9ef67
kevyamon <gamekevy@gmail.com>
2025-07-08 04:31:10 +0000

    vc

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
 import configRoutes from './routes/configRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
-import promotionRoutes from './routes/promotionRoutes.js'; // 1. Importer les routes
+import promotionRoutes from './routes/promotionRoutes.js';
 
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,21 +24,18 @@ app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
 app.use('/api/config', configRoutes);
 app.use('/api/notifications', notificationRoutes);
-app.use('/api/promotions', promotionRoutes); // 2. Utiliser les routes
+app.use('/api/promotions', promotionRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  // On ne sert plus le dossier /uploads car on utilise Cloudinary
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
