87089c7b5442daca0397b94b0ab6d976a6d7647a
kevyamon <gamekevy@gmail.com>
2025-07-08 17:31:39 +0000

    huhu

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ import notificationRoutes from './routes/notificationRoutes.js';
 import promotionRoutes from './routes/promotionRoutes.js';
 
 const port = process.env.PORT || 5000;
+
 connectDB();
+
 const app = express();
 
 app.use(cors());
@@ -21,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Les routes de l'API
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -28,18 +31,10 @@ app.use('/api/config', configRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/promotions', promotionRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  // On ne sert plus le dossier /uploads car on utilise Cloudinary
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+// Route de base pour vérifier que l'API est en ligne
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
