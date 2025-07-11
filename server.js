a15b9b731b433d622f50e4201a3dcd71338db181
kevyamon <gamekevy@gmail.com>
2025-07-11 10:13:31 +0000

    Pri

--- server.js
+++ server.js
@@ -2,43 +2,46 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+dotenv.config();
 import connectDB from './config/db.js';
+
+// On importe uniquement les routes que nous utilisons
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
 import configRoutes from './routes/configRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
 import promotionRoutes from './routes/promotionRoutes.js';
 
-dotenv.config();
 const port = process.env.PORT || 5000;
+
 connectDB();
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Les routes de l'API doivent être déclarées avant les routes de fichiers statiques
+// Les routes de l'API
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
 app.use('/api/config', configRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/promotions', promotionRoutes);
 
+// En mode production, on ne sert que le frontend
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+  // En mode développement, on a une route simple
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
